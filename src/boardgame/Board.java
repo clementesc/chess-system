0a173320422b64367ead4f613d577cb7d0ab2231
Clemente <clementesc@gmail.com>
2020-04-10 15:48:43 -0300

    Início da implementação dos métodos para movimento das peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -46,6 +46,23 @@ public class Board {
         pieces[position.getRow()][position.getColumn()] = piece;
     }
 
+    public Piece removePiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Posição não existe no tabuleiro");
+        }
+
+        if (piece(position) == null) {
+            return null;
+        }
+
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
+
+    }
+
     private boolean positionExists(int row, int column){
         return row >= 0 && column >= 0 && row < rows && column < columns;
     }
