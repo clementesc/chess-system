2c234dde42cc9217236feea18273ceb0cd8bfe4b
Clemente <clementesc@gmail.com>
2020-04-10 18:10:17 -0300

    Implemenado possíveis movimentos da Torre(Rook). Contém erro: java.lang.NullPointerException

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
 
         Piece capturedPiece = makeMove(source, target);
 
@@ -43,6 +44,12 @@ public class ChessMatch {
 
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Não existem movimentos possíveis para peça escolhida.");
+        }
+    }
+
+    private void validateTargetPosition (Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessException("A peça escolhida não pode ser movida para a posição de destino.");
         }
     }
 
