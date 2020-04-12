287d5e71ed4cdb48ef071edf6a84657a842574e6
Clemente <clementesc@gmail.com>
2020-04-12 15:42:28 -0300

    Implementação da peça Rainha

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -8,21 +8,22 @@ import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-    public Pawn(Board board, Color color) {
-        super(board, color);
+	public Pawn(Board board, Color color) {
+		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "P";
 	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
 
-    @Override
-    public boolean[][] possibleMoves() {
-        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-
-        Position p = new Position(0, 0);
-        if (getColor() == Color.WHITE) {
+		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
@@ -47,7 +48,7 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
@@ -59,8 +60,7 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}	
-        }
-        
+		}
 		return mat;
-    }
+	}
 }
