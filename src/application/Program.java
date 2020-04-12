5cbb0782b5c4570411a53f1421e2217d264e3e4b
Clemente <clementesc@gmail.com>
2020-04-12 19:42:43 -0300

    Melhoria no tratamento do movimento especial "promoção"

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,7 +39,11 @@ public class Program {
 
 				if(chessMatch.getPromoted() != null){
 					System.out.print("Informe uma peca para promocao (B/H/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("H") && !type.equals("R") && !type.equals("Q")){
+						System.out.print("Peca invalida. Informe uma peca para promocao (B/H/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -143,7 +142,7 @@ public class ChessMatch {
             throw new IllegalStateException("Nao ha peca para ser promovida");
         }
         if (!type.equals("B") && !type.equals("H") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Peca invalida para promocao");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
